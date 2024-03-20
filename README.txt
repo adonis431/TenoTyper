064d2b720f4ed284f1ad5255070ca52000796947
mcjoh <mcjoh@ADONIS>
2024-03-19 19:16:00 -0700

    Add project files.

---
+++ README.txt
@@ -0,0 +1,11 @@
+
+TennoTyper
+
+this is a translator for the langauges in warframe.
+
+1.0 Notes
+adds english to tenno
+
+
+I do not claim ownership of any langauge(s) from Warframe
+This is not an official project of D.E. nor are they connected to its development.
---
+++ TenoTyper/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TenoTyper
+{
+    public partial class Form1 : Form
+    {
+       
+        String Input = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Input = textBox1.Text;
+
+            textBox2.Text = Input;
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {        
+        
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
